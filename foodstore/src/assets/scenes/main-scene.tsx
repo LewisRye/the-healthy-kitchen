d633acc11af775942b9a8e145b86a27942c19406
Lewis <lewisryework@gmail.com>
2025-03-10 17:49:46 +0000

    Create main scene

---
+++ foodstore/src/assets/scenes/main-scene.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+export const MainScene = () => {
+  const [scene, setScene] = useState("main");
+
+  return (
+    <>
+      {scene === "main" && (
+        <>
+          
+        </>
+      )}
+    </>
+  );
+};
