ea22e573a91a9b7186b588dffc21ffcf58e607d9
Lewis Rye <lewisryework@gmail.com>
2025-03-28 20:38:25 +0000

    Add bootstrap navbar and footer

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / (window.innerHeight * 0.85), 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight * 0.85 );
+const container = document.getElementById('three-container');
+container.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
+window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight * 0.85;
+    renderer.setSize( width, height );
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
