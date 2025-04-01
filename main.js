cfac5134efa0592b1689afb89e176289cc2cc855
Lewis <lewisryework@gmail.com>
2025-04-01 11:02:22 +0100

    Bug fixes for overflow-x

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / (window.innerHeight * 0.85), 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / (window.innerHeight), 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight * 0.85 );
+renderer.setSize( window.innerWidth, window.innerHeight );
 const container = document.getElementById('three-container');
 container.appendChild( renderer.domElement );
 
