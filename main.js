de1a616590162aff8ac7db3fe6109059d1608854
Lewis <lewisryework@gmail.com>
2025-04-02 12:11:25 +0100

    Add beginner GLTF models

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / (window.innerHeight), 0.1, 1000 );
@@ -11,17 +12,29 @@ container.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// scene.add( cube );
 
-camera.position.z = 5;
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+const loader = new GLTFLoader();
+loader.load( '/models/Banana.gltf', function ( gltf ) {
+    console.log( gltf )
+    scene.add( gltf.scene );
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+
+camera.position.z = 150;
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
 
+// a function that checks for window resize events
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
     const height = window.innerHeight * 0.85;
