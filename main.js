ba9c7456513a789255283f13601a2cbc99d5585a
Lewis Rye <lewisryework@gmail.com>
2025-04-02 14:19:46 +0100

    Spinny fruit ahah

--- main.js
+++ main.js
@@ -9,18 +9,33 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 const container = document.getElementById('three-container');
 container.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
 
+let banana;
+let cherry;
+let grape;
+
 const loader = new GLTFLoader();
 loader.load( '/models/Banana.gltf', function ( gltf ) {
-    console.log( gltf )
-    scene.add( gltf.scene );
+    banana = gltf.scene;
+    scene.add( banana );
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+loader.load( '/models/Cherry.gltf', function ( gltf ) {
+    cherry = gltf.scene;
+    cherry.scale.set(15, 15, 15);
+    cherry.position.set(-90, 0, 0);
+    scene.add( cherry );
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+loader.load( '/models/Grape.gltf', function ( gltf ) {
+    grape = gltf.scene;
+    grape.scale.set(10, 10, 10);
+    grape.position.set(90, 0, 0);
+    scene.add( grape );
 }, undefined, function ( error ) {
     console.error( error );
 } );
@@ -28,8 +43,16 @@ loader.load( '/models/Banana.gltf', function ( gltf ) {
 camera.position.z = 150;
 
 function animate() {
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
+    if (banana) {
+        banana.rotation.y += 0.01;
+    }
+    if (cherry) {
+        cherry.rotation.y += 0.01;
+    }
+    if (grape) {
+        grape.rotation.y += 0.01;
+    }
+    
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
@@ -37,7 +60,7 @@ renderer.setAnimationLoop( animate );
 // a function that checks for window resize events
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
-    const height = window.innerHeight * 0.85;
+    const height = window.innerHeight;
     renderer.setSize( width, height );
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
