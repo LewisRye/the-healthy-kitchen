b904cedcb966f4019712001d2ea44dbf367b70f3
Lewis Rye <lewisryework@gmail.com>
2025-04-03 11:32:45 +0100

    Fade in and out of scenes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,46 @@ import { GrapeScene } from "./components/GrapeScene";
 import { StatementScene } from "./components/StatementScene";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
   const [scene, setScene] = useState("home");
+
+  const getSceneComponent = () => {
+    switch (scene) {
+      case "home":
+        return <HomeScene setScene={setScene} />;
+      case "about":
+        return <AboutScene setScene={setScene} />;
+      case "banana":
+        return <BananaScene setScene={setScene} />;
+      case "cherry":
+        return <CherryScene setScene={setScene} />;
+      case "grape":
+        return <GrapeScene setScene={setScene} />;
+      case "statement":
+        return <StatementScene setScene={setScene} />;
+      default:
+        return <HomeScene setScene={setScene} />;
+    }
+  };
 
   return (
     <div>
       <Navbar setScene={setScene} />
 
-      {scene === "home" && <HomeScene setScene={setScene} />}
-      {scene === "about" && <AboutScene setScene={setScene} />}
-      {scene === "banana" && <BananaScene setScene={setScene} />}
-      {scene === "cherry" && <CherryScene setScene={setScene} />}
-      {scene === "grape" && <GrapeScene setScene={setScene} />}
-      {scene === "statement" && <StatementScene setScene={setScene} />}
+      {/* ensure each scene fades in and out */}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={scene}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.25 }}
+        >
+          {getSceneComponent()}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
