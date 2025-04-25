541fb94112deac3666479391076c72ce5f2b8386
Lewis Rye <lewisryework@gmail.com>
2025-04-25 13:07:46 +0100

    Add raycasting

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 var scene,
   camera,
   renderer,
-  clock,
   fridgeMixer,
   fridgeAnimations,
   fridgeActions,
@@ -14,6 +13,7 @@ var scene,
   grapeMixer;
 
 var fridgeOpen = false;
+var lightOn = false;
 var wireframe = false;
 
 scene = new THREE.Scene();
@@ -21,7 +21,7 @@ camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / (window.innerHeight * 0.8),
   0.1,
-  1000
+  10000
 );
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight * 0.8);
@@ -31,20 +31,16 @@ const container = document.getElementById("three-container");
 container.appendChild(renderer.domElement);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 3, 5);
+camera.position.set(-7.5, 3, 5);
 orbit.update();
 
 // add lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-const sunLight = new THREE.DirectionalLight(0xffffff, 1);
+const sunLight = new THREE.DirectionalLight(0xffffff, 0.75);
 sunLight.position.set(5, 10, 5);
 sunLight.castShadow = true;
-sunLight.shadow.mapSize.width = 1024;
-sunLight.shadow.mapSize.height = 1024;
-sunLight.shadow.camera.near = 0.5;
-sunLight.shadow.camera.far = 50;
 scene.add(sunLight);
 
 // add floor
@@ -53,8 +49,8 @@ const floorTexture = textureLoader.load(
   "https://dl.polyhaven.org/file/ph-assets/Textures/png/1k/granite_tile/granite_tile_rough_1k.png"
 );
 floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-floorTexture.repeat.set(10, 10);
-const floorGeometry = new THREE.PlaneGeometry(25, 25);
+floorTexture.repeat.set(2.5, 3.125);
+const floorGeometry = new THREE.PlaneGeometry(10, 12.5);
 const floorMaterial = new THREE.MeshLambertMaterial({
   map: floorTexture,
 });
@@ -62,27 +58,72 @@ const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.rotation.x = -Math.PI / 2;
 scene.add(floor);
 
-// for debugging only
-// const gridHelper = new THREE.GridHelper(12, 12);
-// scene.add(gridHelper);
+// add back wall
+const backWallGeometry = new THREE.PlaneGeometry(10, 5);
+const backWallMaterial = new THREE.MeshLambertMaterial({ color: 0xfbfdf8 });
+const backWall = new THREE.Mesh(backWallGeometry, backWallMaterial);
+backWall.position.set(0, 2.5, -6.2);
+scene.add(backWall);
 
-// for debugging only
-// const axesHelper = new THREE.AxesHelper(4);
-// scene.add(axesHelper);
+// add right wall
+const rightWallGeometry = new THREE.PlaneGeometry(12.5, 5);
+const rightWallMaterial = new THREE.MeshLambertMaterial({ color: 0xfbfdf8 });
+const rightWall = new THREE.Mesh(rightWallGeometry, rightWallMaterial);
+rightWall.position.set(5, 2.5, 0);
+rightWall.rotation.y = -Math.PI / 2;
+scene.add(rightWall);
+
+// add door to right wall
+const boxDoorGeometry = new THREE.BoxGeometry(1.8, 3, 0.1);
+const boxDoorMaterial = new THREE.MeshLambertMaterial({ color: 0x8b4513 });
+const boxDoor = new THREE.Mesh(boxDoorGeometry, boxDoorMaterial);
+boxDoor.position.set(4.95, 1.5, 5);
+boxDoor.rotation.y = -Math.PI / 2;
+scene.add(boxDoor);
+
+// add door handle
+const handleGeometry = new THREE.CylinderGeometry(0.05, 0.05, 0.4, 32);
+const handleMaterial = new THREE.MeshLambertMaterial({ color: 0x333333 });
+const handle = new THREE.Mesh(handleGeometry, handleMaterial);
+handle.rotation.z = Math.PI / 2;
+handle.position.set(4.85, 1.5, 4.2);
+scene.add(handle);
+
+// add light switch
+const toggleGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.25);
+const toggleMaterial = new THREE.MeshLambertMaterial({ color: 0x111111 });
+const lightSwitch = new THREE.Mesh(toggleGeometry, toggleMaterial);
+lightSwitch.rotation.z = -20; // -20 for off, 20 for on
+lightSwitch.position.set(5, 1.75, 3.5);
+scene.add(lightSwitch);
 
 // load all models
 let fridge;
+const fridgeMesh = [];
+
 let banana;
+const bananaMesh = [];
+
 let cherry;
+const cherryMesh = [];
+
 let grape;
+const grapeMesh = [];
 
 const loader = new GLTFLoader();
 loader.load(
   "/models/fridge.glb",
   function (gltf) {
     fridge = gltf.scene;
-    fridge.position.set(0, 1.5, 0);
+    fridge.position.set(0, 1.5, -5.5);
     scene.add(fridge);
+
+    // for raycasting
+    fridge.traverse(function (child) {
+      if (child.isMesh) {
+        fridgeMesh.push(child);
+      }
+    });
 
     // for animations
     fridgeMixer = new THREE.AnimationMixer(gltf.scene);
@@ -101,9 +142,16 @@ loader.load(
   function (gltf) {
     banana = gltf.scene;
     banana.scale.set(0.01, 0.01, 0.01);
-    banana.position.set(0, 1.5, 0);
+    banana.position.set(0, 1.5, -5.5);
     banana.rotateZ(80);
     scene.add(banana);
+
+    // for raycasting
+    banana.traverse(function (child) {
+      if (child.isMesh) {
+        bananaMesh.push(child);
+      }
+    });
 
     // for animations
     bananaMixer = new THREE.AnimationMixer(banana);
@@ -123,8 +171,15 @@ loader.load(
   function (gltf) {
     cherry = gltf.scene;
     cherry.scale.set(0.09, 0.09, 0.09);
-    cherry.position.set(0, 0.75, 0);
+    cherry.position.set(0, 0.75, -5.5);
     scene.add(cherry);
+
+    // for raycasting
+    cherry.traverse(function (child) {
+      if (child.isMesh) {
+        cherryMesh.push(child);
+      }
+    });
 
     // for animations
     cherryMixer = new THREE.AnimationMixer(cherry);
@@ -144,8 +199,15 @@ loader.load(
   function (gltf) {
     grape = gltf.scene;
     grape.scale.set(0.05, 0.04, 0.05);
-    grape.position.set(0, 0.1, 0);
+    grape.position.set(0, 0.1, -5.5);
     scene.add(grape);
+
+    // for raycasting
+    grape.traverse(function (child) {
+      if (child.isMesh) {
+        grapeMesh.push(child);
+      }
+    });
 
     // for animations
     grapeMixer = new THREE.AnimationMixer(grape);
@@ -161,29 +223,39 @@ loader.load(
   }
 );
 
-// open fridge button
-document.getElementById("btnOpenFridge").addEventListener("click", function () {
-  if (fridgeActions.length > 0 && !fridgeOpen) {
-    for (let action of fridgeActions) {
-      action.reset();
-      action.setLoop(THREE.LoopOnce);
-      action.clampWhenFinished = true;
-      action.timeScale = 1;
-      action.play();
-
-      setTimeout(() => {
-        action.paused = true;
-        action.time = 1.33;
-      }, 1900);
-    }
-    fridgeOpen = true;
-  }
-});
-
-// close fridge button
+// toggle wireframe button
 document
-  .getElementById("btnCloseFridge")
+  .getElementById("btnToggleWireframe")
   .addEventListener("click", function () {
+    wireframe = !wireframe;
+    scene.traverse(function (object) {
+      if (object.isMesh) {
+        object.material.wireframe = wireframe;
+      }
+    });
+  });
+
+const clock = new THREE.Clock();
+const pointer = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+raycaster.far = 1000;
+raycaster.near = 0.1;
+
+window.addEventListener("click", onMouseClick, false);
+
+function onMouseClick(event) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(pointer, camera);
+
+  const allIntersects = getIntersections(pointer);
+
+  // fridge clicked
+  if (
+    allIntersects.length > 0 &&
+    fridgeMesh.includes(allIntersects[0].object)
+  ) {
     if (fridgeActions.length > 0 && fridgeOpen) {
       for (let action of fridgeActions) {
         action.reset();
@@ -199,26 +271,54 @@ document
         }, 2000);
       }
       fridgeOpen = false;
-    }
-  });
+    } else if (fridgeActions.length > 0 && !fridgeOpen) {
+      for (let action of fridgeActions) {
+        action.reset();
+        action.setLoop(THREE.LoopOnce);
+        action.clampWhenFinished = true;
+        action.timeScale = 1;
+        action.play();
 
-// toggle wireframe button
-document
-  .getElementById("btnToggleWireframe")
-  .addEventListener("click", function () {
-    console.log("clicked wireframe button");
-    wireframe = !wireframe;
-    scene.traverse(function (object) {
-      if (object.isMesh) {
-        object.material.wireframe = wireframe;
+        setTimeout(() => {
+          action.paused = true;
+          action.time = 1.33;
+        }, 1900);
       }
-    });
-  });
+      fridgeOpen = true;
+    }
+  }
 
-clock = new THREE.Clock();
+  const lightSwitchIntersects = raycaster.intersectObject(lightSwitch);
+
+  // light switch clicked
+  if (lightSwitchIntersects.length > 0) {
+    lightOn = !lightOn;
+    lightSwitch.rotation.z = -lightSwitch.rotation.z;
+
+    if (lightOn) {
+      renderer.setClearColor(0x303236);
+    } else {
+      renderer.setClearColor(0x87ceeb);
+    }
+  }
+}
+
+function getIntersections(pointer) {
+  raycaster.setFromCamera(pointer, camera);
+  return raycaster.intersectObject(fridge, true);
+}
+
+window.addEventListener("resize", () => {
+  // resize camera to allow for new window size
+  var width = window.innerWidth;
+  var height = window.innerHeight * 0.8;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
 
 function animate() {
-  const delta = clock.getDelta();
+  let delta = clock.getDelta();
 
   if (fridgeMixer) fridgeMixer.update(delta);
   if (bananaMixer) bananaMixer.update(delta);
@@ -229,12 +329,3 @@ function animate() {
 }
 
 renderer.setAnimationLoop(animate);
-
-// a function that checks for window resize events
-window.addEventListener("resize", () => {
-  var width = window.innerWidth;
-  var height = window.innerHeight * 0.8;
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-});
